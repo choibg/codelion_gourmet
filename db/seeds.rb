80d609e6b902510b0ff06e57e4757920d34a6fd6
Kilho <catharsistk@gmail.com>
2014-02-21 02:01:02 +0900

    시드 추가하기

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = ["korean", "chinese", "japanese", "western"]
+
+categories.each do |category|
+	1.upto(3) do |post|
+		p = Post.new
+		p.user_id = 1
+		p.category = category
+		p.title = "제목"
+		p.content = "소개글이 들어가는 부분"
+		p.like = 0
+		p.save
+		puts p.title
+	end
+end
