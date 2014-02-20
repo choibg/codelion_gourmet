e4a47c3910cd4203ca972f1c12381b764862670a
Kilho <catharsistk@gmail.com>
2014-02-21 00:50:29 +0900

    User 테이블의 마이그레이션 파일, 모델 파일 내용 추가

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+	has_many	:comments
+	has_many	:posts
+	validates :username, :presence => { :message => "Username cannot be blank." }
+	validates :username, :uniqueness => { :message => "Username is already taken.", :case_sensitive => false }
+	validates :password, :length => { :minimum => 6, :too_short => "Your password must be at least 6 characters long." }
 end
